0f909539e885a3f7e87244bc781fabf74dfa262e
Reinaldo <reinaldo.riant@kompas.com>
2021-07-08 01:09:04 +0700

    + utils center moodifier

---
+++ Africa/Modifier/ContentModifier.swift
@@ -0,0 +1,20 @@
+//
+//  ContentModifier.swift
+//  Africa
+//
+//  Created by TI Digital on 08/07/21.
+//
+
+import SwiftUI
+
+struct ContentModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentModifier()
+    }
+}
