920d978a05a83900bca4d0a70f342a2db414e422
Reinaldo <reinaldo.riant@kompas.com>
2021-07-07 15:34:01 +0700

    + add navigation bottom view

---
+++ Africa/App/MainView.swift
@@ -0,0 +1,39 @@
+//
+//  MainView.swift
+//  Africa
+//
+//  Created by TI Digital on 07/07/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView{
+            ContentView().tabItem {
+                Image(systemName: "square.grid.2x2")
+                Text("Browse")
+            }
+            VideoListView()
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+            MapView().tabItem {
+                Image(systemName: "map")
+                Text("Locations")
+            }
+            GalleryView().tabItem {
+                Image(systemName: "photo")
+                Text("Gallery")
+            }
+        }
+        
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
