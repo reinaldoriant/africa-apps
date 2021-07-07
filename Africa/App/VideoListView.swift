89739de873044da2d3d7d3f80edf929cabc6db7b
Reinaldo <reinaldo.riant@kompas.com>
2021-07-07 21:13:56 +0700

    ! modify video list and add shuffle

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct VideoListView: View {
     
-    var videos: [Video] = Bundle.main.decode("videos.json")
+   @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
     
     var body: some View {
         NavigationView {
@@ -21,6 +23,17 @@ struct VideoListView: View {
             }//: list
             .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Videos",displayMode: .inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: {
+                        // shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }){
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
         }//: navigation
     }
 }
