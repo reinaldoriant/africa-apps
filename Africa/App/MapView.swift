4a61a039f0d6744fb2c2b665791b8ddcec8d3b9e
Reinaldo <reinaldo.riant@kompas.com>
2021-07-07 15:33:43 +0700

    + add view

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by TI Digital on 07/07/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
