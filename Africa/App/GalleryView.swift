9d90df86d88d066862b7d8e07169a80933d52169
Reinaldo <reinaldo.riant@kompas.com>
2021-07-07 23:40:11 +0700

    ! Efficient grid layout

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,30 +9,47 @@ import SwiftUI
 
 struct GalleryView: View {
     
+    @State private var selectedAnimal: String = "lion"
     let animals: [Animal] = Bundle.main.decode("animals.json")
     
     //SIMPLE : GRID DEFINITION
     
-    let gridLayout: [GridItem] = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    //EFFICIENT : Grid definition
+    
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
     
     var body: some View {
         ScrollView(.vertical,showsIndicators: false){
-            //MARK : GRID
-            
-            
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
-                ForEach(animals) { item in
-                    Image(item.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                }
-            }
+            VStack(alignment: .center, spacing: 30) {
+                //MARK : IMAGE
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                //MARK : GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    }//: loop
+                }//: grid
+            }//: vstack
+            .padding(.horizontal,10)
+            .padding(.vertical,50)
         }//: Scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView() )
