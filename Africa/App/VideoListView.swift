d3ba28bb154ce84306a299cd2f34402236286925
Reinaldo <reinaldo.riant@kompas.com>
2021-07-07 21:00:31 +0700

    ! connecting list view with list item

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List{
+                ForEach(videos) { item in
+                    VideoListItem(video: item)
+                }
+            }//: list
+        }//: navigation
     }
 }
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
+            .previewDevice("iPhone 11")
     }
 }
